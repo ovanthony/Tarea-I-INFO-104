5e07903d76c735a20caaa2cb1b3f759e02f9d537
ovanthony <anthonyov27@gmail.com>
2023-01-31 21:32:21 -0600

    Tarea I del curso.

---
+++ Fundamentos de Programación/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos_de_Programación
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // Tipos de datos
+
+            int num1 = 214748364;
+            uint valo1 = 0;
+            int num2 = 5, num3 = 6;
+            long largas = 21474836488888;
+            ulong ularga = 2222;
+            byte b = 100;
+            sbyte s = 23;
+            int? total = null;
+
+            float flotante = 25.36f;
+            double doble = 100.5d;
+            decimal dec = 56.65m;
+
+            bool boleana = new bool();
+
+            // var & dynamic
+
+            var variable = 20.03f;
+            variable = 20;
+
+            dynamic dinamico = "Hoy es un buen dia";
+            dinamico = 20.699f;
+            dinamico = true;
+
+            
+            var persona = new { NOMBRE = "Miguel", APELLIDO = "Rios", Edad = 30 };
+
+            Console.WriteLine($"{persona.NOMBRE} {persona.APELLIDO} {persona.Edad}");
+
+            string nombre1 = "Carolina";
+            string nombre2 = "Luis Carlos";
+            String apellido1 = "Rodriguez";
+            String apellido2 = "Morera";
+            char caracter = 'c';
+
+            Console.WriteLine("Total: " + total + "Boleana: " + boleana);
+            Console.WriteLine(nombre1 + " " + apellido1);
+            Console.WriteLine(nombre2 + " " + apellido2);
+
+            //Desplegar con parametros
+            Console.WriteLine("La estudiante se llama {0} {1}", nombre1, apellido1);
+            //Desplegar con interpolaion
+            Console.WriteLine($"El estudiante se llama {nombre2} {apellido2}\r\n");
+            
+
+            Console.WriteLine("Digite un numero:");
+            Console.ForegroundColor = ConsoleColor.White;
+            num1 = (int)float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite otro numero");
+            num2 = (int)float.Parse(Console.ReadLine());
+            total = num1 + num2;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"La sumatoria de {num1} + {num2} es {total} ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"La resta de {num1} - {num2} es {num1 - num2} ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"La multiplicacion de {num1} * {num2} es {num1 * num2} ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"La division de {num1} / {num2} es {num1 / num2} \r\n");
+            */
+
+            // Datos
+            int[] edad = new int[] {20, 22, 33};
+            float[] salarios = new float[] {200, 300, 150};
+            String[] musico = new String[3];
+            musico[0] = "Anthony";
+            musico[1] = "Helen";
+            musico[2] = "Jose";
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Salarios de los músicos:");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            // Ciclo For
+            for (int i = 0; i < musico.Length; i++)
+            {
+                float IVA = 0;
+                IVA = (float)(salarios[i]*0.13);
+                Console.WriteLine($"{musico[i]}, edad {edad[i]}, salario {salarios[i]}$. IVA: {IVA}, total de {salarios[i]-IVA}$.");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Fundamentos de Programación/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Fundamentos de Programación")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fundamentos de Programación")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("8aacf409-7c57-4bf1-8876-98df65f8ee71")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
